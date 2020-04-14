8467bba61cb3698487403c54e41b0c831ebc183f
secret <sufay30281@gmail.com>
2020-04-14 17:31:24 +0800

    add keeper

--- types/keys.go
+++ types/keys.go
@@ -13,3 +13,15 @@ const (
 	// RouterKey is the msg router key for the service module
 	RouterKey string = ModuleName
 )
+
+var (
+	// Keys for store prefixes
+
+	ServiceDefinitionKey = []byte{0x11} // prefix for service definition
+)
+
+// GetServiceDefinitionKey gets the key for the service definition with the specified service name
+// VALUE: service/ServiceDefinition
+func GetServiceDefinitionKey(serviceName string) []byte {
+	return append(ServiceDefinitionKey, []byte(serviceName)...)
+}
