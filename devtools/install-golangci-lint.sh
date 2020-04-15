204314bf895725781485fed18c7bd2ae9134112f
secret <sufay30281@gmail.com>
2020-04-15 15:38:11 +0800

    remove debug echo

--- devtools/install-golangci-lint.sh
+++ devtools/install-golangci-lint.sh
@@ -26,7 +26,6 @@ echo "Downloading golangci-lint ${VERSION} installer ..." >&2
 "${CURL}" -sfL "https://raw.githubusercontent.com/golangci/golangci-lint/${VERSION}/install.sh" > "${installer}"
 
 echo "Checking hashsum ..." >&2
-echo "$(f_sha256 ${installer})"
 [ "${HASHSUM}" = "$(f_sha256 ${installer})" ]
 chmod +x "${installer}"
 
