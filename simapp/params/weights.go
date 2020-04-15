6506b70450e5845d8af260538c4a12dea0129568
secret <sufay30281@gmail.com>
2020-04-15 11:27:28 +0800

    remove weights

--- simapp/params/weights.go
+++ simapp/params/weights.go
@@ -2,15 +2,5 @@ package params
 
 // Default simulation operation weights for messages and gov proposals
 const (
-	DefaultWeightMsgCreateValidator int = 0
-	DefaultWeightMsgEditValidator   int = 0
-	DefaultWeightMsgDelegate        int = 0
-	DefaultWeightMsgUndelegate      int = 0
-	DefaultWeightMsgBeginRedelegate int = 0
-
-	DefaultWeightCommunitySpendProposal int = 5
-	DefaultWeightTextProposal           int = 5
-	DefaultWeightParamChangeProposal    int = 5
-
 	DefaultWeightMsgDefineService int = 100
 )
--- simulation/operations.go
+++ simulation/operations.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 
