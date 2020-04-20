a0a21a20314fa77bc6e989ee86e225147ddcef71
secret2830 <sufay30281@gmail.com>
2020-04-21 00:07:02 +0800

    add simulation

--- simapp/params/weights.go
+++ simapp/params/weights.go
@@ -1,6 +1,12 @@
 package params
 
-// Default simulation operation weights for messages and gov proposals
+// Default simulation operation weights for messages
 const (
-	DefaultWeightMsgDefineService int = 100
+	DefaultWeightMsgDefineService         int = 100
+	DefaultWeightMsgBindService           int = 100
+	DefaultWeightMsgUpdateServiceBinding  int = 100
+	DefaultWeightMsgSetWithdrawAddress    int = 100
+	DefaultWeightMsgDisableServiceBinding int = 100
+	DefaultWeightMsgEnableServiceBinding  int = 100
+	DefaultWeightMsgRefundServiceDeposit  int = 100
 )
