eb6a528c67c6e64545b6372e97bd6e1b4b1adba0
chengwenxi <vincent.ch.cn@gmail.com>
2020-07-10 14:31:27 +0800

    add endblock

--- module.go
+++ module.go
@@ -138,7 +138,8 @@ func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
 
 // EndBlock returns the end blocker for the service module. It returns no validator
 // updates.
-func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	EndBlocker(ctx, am.keeper)
 	return []abci.ValidatorUpdate{}
 }
 
